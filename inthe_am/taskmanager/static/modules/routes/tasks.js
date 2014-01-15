5835f9909e76e5d292eb9e8cf9b0d284767c1dcd
Adam Coddington <me@adamcoddington.net>
2014-01-14 18:21:22 -0800

    Forcibly unload tasks in 'before model'.

--- inthe_am/taskmanager/static/modules/routes/tasks.js
+++ inthe_am/taskmanager/static/modules/routes/tasks.js
@@ -2,6 +2,9 @@ var route = Ember.Route.extend({
   model: function(){
     return this.store.findQuery('task', {completed: 0});
   },
+  beforeModel: function() {
+    this.store.unloadAll(App.Task);
+  },
   afterModel: function(tasks, transition) {
     if (tasks.get('length') === 0) {
       this.transitionTo('getting_started');
