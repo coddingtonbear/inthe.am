5ade3f78c8ad170714eab8018ca83856dbb29348
Adam Coddington <me@adamcoddington.net>
2014-01-19 09:13:51 -0800

    Only transition to first object if destination route is task.index.

--- inthe_am/taskmanager/static/task_manager.js
+++ inthe_am/taskmanager/static/task_manager.js
@@ -396,7 +396,7 @@ var route = Ember.Route.extend({
   afterModel: function(tasks, transition) {
     if (tasks.get('length') === 0) {
       this.transitionTo('getting_started');
-    } else {
+    } else if (transition.targetName == "task.index") {
       this.transitionTo('task', tasks.get('firstObject'));
     }
   }
