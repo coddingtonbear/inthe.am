b341a587ed3f1af7734fb09eaaf049346489b6f1
Adam Coddington <me@adamcoddington.net>
2014-02-03 21:36:16 -0800

    Removing extra console.log.

--- inthe_am/taskmanager/static/modules/controllers/tasks.js
+++ inthe_am/taskmanager/static/modules/controllers/tasks.js
@@ -2,7 +2,6 @@ var controller = Ember.ArrayController.extend({
   sortProperties: ['urgency'],
   sortAscending: false,
   refresh: function(){
-    console.log("Updating tasks...");
     this.get('content').update();
   }
 });
