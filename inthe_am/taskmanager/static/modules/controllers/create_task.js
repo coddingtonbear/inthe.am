685a97d948e060f86ea496243bf5d284da72de9d
Adam Coddington <me@adamcoddington.net>
2014-02-09 13:26:34 -0800

    Immediately close the form rather than waiting for save results.

--- inthe_am/taskmanager/static/modules/controllers/create_task.js
+++ inthe_am/taskmanager/static/modules/controllers/create_task.js
@@ -10,8 +10,8 @@ var controller = Ember.ObjectController.extend({
     'save': function() {
       var model = this.get('model');
       var self = this;
+      $('#new_task_form').foundation('reveal', 'close');
       model.save().then(function(){
-        $('#new_task_form').foundation('reveal', 'close');
         self.transitionToRoute('task', model);
       }, function(){
         alert("An error was encountered while saving your task.");
