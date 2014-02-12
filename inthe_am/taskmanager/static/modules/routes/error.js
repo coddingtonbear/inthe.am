e3e5494da8db3375509be29bc2bdf85816ecb345
Adam Coddington <me@adamcoddington.net>
2014-02-11 17:49:47 -0800

    Redirect in the next run after reporting.

--- inthe_am/taskmanager/static/modules/routes/error.js
+++ inthe_am/taskmanager/static/modules/routes/error.js
@@ -2,11 +2,13 @@ var route = Ember.Route.extend({
   renderTemplate: function(_, error){
     this._super();
     var self = this;
-    //setTimeout(function(){
-    //  self.controllerFor('application').reportError(error);
-    //  var url = self.controllerFor('application').urls.about;
-    //  window.location = url;
-    //}, 3000);
+    Ember.run.once(_, function(){
+      self.controllerFor('application').reportError(error);
+      Ember.run.next(_, function(){
+        var url = self.controllerFor('application').urls.about;
+        window.location = url;
+      });
+    });
   },
 });
 
