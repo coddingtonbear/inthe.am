05c7f82d3c6a524db97239c5faee31ab84fe8106
Adam Coddington <me@adamcoddington.net>
2014-02-11 17:01:13 -0800

    Issue 36: Run next, not once.

--- inthe_am/taskmanager/static/modules/routes/error.js
+++ inthe_am/taskmanager/static/modules/routes/error.js
@@ -2,7 +2,7 @@ var route = Ember.Route.extend({
   renderTemplate: function(_, error){
     this._super();
     var self = this;
-    Ember.run.once(self, function(){
+    Ember.run.next(self, function(){
       self.controllerFor('application').reportError(error);
       var url = self.controllerFor('application').urls.about;
       window.location = url;
