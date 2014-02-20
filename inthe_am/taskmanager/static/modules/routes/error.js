60513df701b408f10b7c0d8f3d1da6d740658186
Adam Coddington <me@adamcoddington.net>
2014-02-20 17:47:14 -0500

    Adding a slightly better error message page.

--- inthe_am/taskmanager/static/modules/routes/error.js
+++ inthe_am/taskmanager/static/modules/routes/error.js
@@ -4,6 +4,9 @@ var route = Ember.Route.extend({
     var self = this;
     Ember.run.once(_, function(){
       reportError(error);
+      Ember.run.next(_, function(){
+        setTimeout(3000, window.location.reload);
+      });
     });
   },
 });
