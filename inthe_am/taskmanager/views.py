921eb9221f976ecaecf637b01c5586eb97991d92
Adam Coddington <me@adamcoddington.net>
2014-03-08 19:07:49 -0800

    Use a smarter change detection algorithm for local taskd users.

--- inthe_am/taskmanager/views.py
+++ inthe_am/taskmanager/views.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import re
 import time
 
@@ -46,6 +47,25 @@ class Status(BaseSseView):
 
         return changed_tickets
 
+    def check_head(self, head):
+        store = self.get_store()
+        new_head = store.repository.head()
+        if head != new_head:
+            logger.info('Found new repository head -- %s' % new_head)
+            ids = self.get_changed_ids(store, head, new_head)
+            for id in ids:
+                self.sse.add_message("task_changed", id)
+            head = new_head
+            self.sse.add_message("head_changed", new_head)
+        return head
+
+    def get_taskd_mtime(self, store):
+        try:
+            taskd_mtime = os.path.getmtime(store.taskd_data_path)
+        except OSError:
+            taskd_mtime = 0
+        return taskd_mtime
+
     def iterator(self):
         store = self.get_store()
         if not store:
@@ -53,28 +73,31 @@ class Status(BaseSseView):
         store.sync()
         created = time.time()
         last_sync = time.time()
+        taskd_mtime = self.get_taskd_mtime(store)
         head = self.request.GET.get('head', store.repository.head())
         while time.time() - created < settings.EVENT_STREAM_TIMEOUT:
-            if time.time() - last_sync > (
-                settings.EVENT_STREAM_POLLING_INTERVAL
-            ):
-                last_sync = time.time()
-                store.sync()
-
-            store = self.get_store()
-            new_head = store.repository.head()
-            if head != new_head:
-                logger.info('Found new repository head -- %s' % new_head)
-                ids = self.get_changed_ids(store, head, new_head)
-                for id in ids:
-                    self.sse.add_message("task_changed", id)
-                head = new_head
-                self.sse.add_message("head_changed", new_head)
+            if store.using_local_taskd:
+                loop_interval = settings.EVENT_STREAM_LOCAL_LOOP_INTERVAL
+                new_mtime = self.get_taskd_mtime(store)
+                if new_mtime != taskd_mtime:
+                    taskd_mtime = new_mtime
+                    store.sync()
+                    head = self.check_head(head)
             else:
-                self.sse.add_message("heartbeat", str(time.time()))
+                loop_interval = settings.EVENT_STREAM_LOOP_INTERVAL
+                if time.time() - last_sync > (
+                    settings.EVENT_STREAM_POLLING_INTERVAL
+                ):
+                    last_sync = time.time()
+                    store.sync()
+
+                head = self.check_head(head)
+
+            self.sse.add_message("heartbeat", str(time.time()))
 
             yield
-            time.sleep(5)
+
+            time.sleep(loop_interval)
 
 
 def home(request):
