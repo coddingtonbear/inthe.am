24dfbd312b8a36f2563733c71fa2b78870623457
Adam Coddington <me@adamcoddington.net>
2014-04-15 12:15:25 -0700

    Import models relatively.

--- inthe_am/taskmanager/tasks.py
+++ inthe_am/taskmanager/tasks.py
@@ -3,11 +3,11 @@ from __future__ import absolute_import
 from celery import shared_task
 
 from .taskwarrior_client import TaskwarriorError
-from .models import TaskStore
 
 
 @shared_task
 def sync_repository(store_id):
+    from .models import TaskStore
     store = TaskStore.objects.get(pk=store_id)
     try:
         store.sync(async=False)
