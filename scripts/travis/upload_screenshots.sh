7d0140124cd851ccb61b81e12c52d06ae6613487
Adam Coddington <me@adamcoddington.net>
2014-04-20 14:22:22 -0700

    Zip up assets before uploading.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -1,15 +1,5 @@
-for screenshot in /tmp/*.png
-do
-    travis-artifacts upload --path "$screenshot"
-done
-for screenshot in /tmp/*.html
-do
-    travis-artifacts upload --path "$screenshot"
-done
-for screenshot in /tmp/*.log
-do
-    travis-artifacts upload --path "$screenshot"
-done
+tar -cvzf /tmp/testing_artifacts.tar.gz /tmp/*.log /tmp/*.html /tmp/*.png
+travis-artifacts upload --path /tmp/testing_artifacts.tar.gz
 tar -cvzf /tmp/task_data.tar.gz task_data
 travis-artifacts upload --path /tmp/task_data.tar.gz
 tar -cvzf /tmp/database.tar.gz db.sqlite3
