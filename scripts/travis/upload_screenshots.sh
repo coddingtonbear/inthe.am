341b755592fe8b2297e9c7589e79dc5e2d606ea8
Adam Coddington <me@adamcoddington.net>
2014-04-20 14:37:42 -0700

    Changing the upload_screenshots script slightly.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -1,6 +1,6 @@
-tar -cvzf /tmp/testing_artifacts.tar.gz /tmp/*.log /tmp/*.html /tmp/*.png
+tar -czf /tmp/testing_artifacts.tar.gz --exclude '*.tar.gz'
 travis-artifacts upload --path /tmp/testing_artifacts.tar.gz
-tar -cvzf /tmp/task_data.tar.gz task_data
+tar -czf /tmp/task_data.tar.gz task_data
 travis-artifacts upload --path /tmp/task_data.tar.gz
-tar -cvzf /tmp/database.tar.gz db.sqlite3
+tar -czf /tmp/database.tar.gz db.sqlite3
 travis-artifacts upload --path /tmp/database.tar.gz
