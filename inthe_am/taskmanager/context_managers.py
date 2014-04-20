d166dda874b9d2487db6208936bd3a69c76c7d0b
Adam Coddington <me@adamcoddington.net>
2014-04-20 11:21:22 -0700

    Raise error directly if on travis.

--- inthe_am/taskmanager/context_managers.py
+++ inthe_am/taskmanager/context_managers.py
@@ -39,46 +39,47 @@ def git_checkpoint(
                     kwargs=kwargs
                 )
             except Exception as e:
-                store.create_git_checkpoint(
-                    str(e),
-                    function=function,
-                    args=args,
-                    kwargs=kwargs,
-                    rollback=True
-                )
-                try:
-                    dangling_sha = store.repository.head()
-                except KeyError:
-                    dangling_sha = None
-                changes_were_stored = (
-                    dangling_sha and dangling_sha != pre_work_sha
-                )
-                if changes_were_stored:
-                    logger.exception(
-                        "An error occurred that required rolling-back "
-                        "the git repository at %s from %s to %s.",
-                        store.local_path,
-                        dangling_sha,
-                        pre_work_sha,
+                if 'TRAVIS' not in os.environ:
+                    store.create_git_checkpoint(
+                        str(e),
+                        function=function,
+                        args=args,
+                        kwargs=kwargs,
+                        rollback=True
                     )
-                    store.log_error(
-                        "An error occurred while interacting with your task "
-                        "list, and your task list was recovered by "
-                        "rolling-back to the last known good state (%s).  "
-                        "Since your task list is synchronized with a taskd "
-                        "server, this will likely not have any negative "
-                        "effects.  Rollback ID: %s.",
-                        pre_work_sha,
-                        dangling_sha,
+                    try:
+                        dangling_sha = store.repository.head()
+                    except KeyError:
+                        dangling_sha = None
+                    changes_were_stored = (
+                        dangling_sha and dangling_sha != pre_work_sha
                     )
-                    store.git_reset(pre_work_sha)
-                else:
-                    logger.exception(
-                        "An error occured that did not require rolling-back "
-                        "the git repository at %s (at %s)",
-                        store.local_path,
-                        pre_work_sha
-                    )
+                    if changes_were_stored:
+                        logger.exception(
+                            "An error occurred that required rolling-back "
+                            "the git repository at %s from %s to %s.",
+                            store.local_path,
+                            dangling_sha,
+                            pre_work_sha,
+                        )
+                        store.log_error(
+                            "An error occurred while interacting with your task "
+                            "list, and your task list was recovered by "
+                            "rolling-back to the last known good state (%s).  "
+                            "Since your task list is synchronized with a taskd "
+                            "server, this will likely not have any negative "
+                            "effects.  Rollback ID: %s.",
+                            pre_work_sha,
+                            dangling_sha,
+                        )
+                        store.git_reset(pre_work_sha)
+                    else:
+                        logger.exception(
+                            "An error occured that did not require rolling-back "
+                            "the git repository at %s (at %s)",
+                            store.local_path,
+                            pre_work_sha
+                        )
                 raise
     except LockTimeout:
         lockfile_created = datetime.datetime.fromtimestamp(
