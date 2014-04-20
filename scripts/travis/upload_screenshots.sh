20dc9755a7fbd1797ebb012f3198d1b4a88763ea
Adam Coddington <me@adamcoddington.net>
2014-04-20 11:37:21 -0700

    Also upload testing database.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -8,3 +8,5 @@ do
 done
 tar -cvzf /tmp/task_data.tar.gz task_data
 travis-artifacts upload --path /tmp/task_data.tar.gz
+tar -cvzf /tmp/database.tar.gz db.sqlite3
+travis-artifacts upload --path /tmp/database.tar.gz
