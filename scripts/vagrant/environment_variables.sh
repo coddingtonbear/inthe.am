aeb15fb740f48e223d4473cd4840035f8cc79053
Adam Coddington <me@adamcoddington.net>
2014-04-19 17:26:33 -0700

    Reorganizing environment variables file.

--- scripts/vagrant/environment_variables.sh
+++ scripts/vagrant/environment_variables.sh
@@ -2,10 +2,13 @@
 # handle log-in correctly
 export TWWEB_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY=enter your key
 export TWWEB_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET=enter your secret
+
 # If you would like to be able to run integration tests, you'll need
 # to create a dummy google account, too
 export TWWEB_TESTING_LOGIN_USER=enter your testing account username
 export TWWEB_TESTING_LOGIN_PASSWORD=enter your testing account password
+
+# The below settings are unlikely to need to be changed for testing.
 export TWWEB_TASKD_BINARY=/usr/local/bin/taskd
 export TWWEB_TASKD_SERVER=127.0.0.1:53589
 export TWWEB_TASKD_ORG=inthe_am
@@ -14,6 +17,7 @@ export TWWEB_TASKD_DATA=/var/taskd
 export TWWEB_TASKD_SIGNING_TEMPLATE=/var/taskd/cert.template
 export DJANGO_SETTINGS_MODULE=inthe_am.settings
 export PYTHONPATH=/var/www/twweb
+
 # To more-accurately match production, set the following:
 #export TWWEB_DATABASE_ENGINE=django.db.backends.postgresql_psycopg2
 #export TWWEB_DATABASE_NAME=<your dbname>
