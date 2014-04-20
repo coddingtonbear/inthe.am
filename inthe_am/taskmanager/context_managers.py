ef8e2aab67cb959953c7620699e7a472bf023886
Adam Coddington <me@adamcoddington.net>
2014-04-20 00:25:46 -0700

    If an error occurs while rolling back, let's not just leave it on the floor like a forlorn cat poop.

--- inthe_am/taskmanager/context_managers.py
+++ inthe_am/taskmanager/context_managers.py
@@ -38,42 +38,47 @@ def git_checkpoint(
                     kwargs=kwargs
                 )
             except Exception as e:
-                store.create_git_checkpoint(
-                    str(e),
-                    function=function,
-                    args=args,
-                    kwargs=kwargs,
-                    rollback=True
-                )
-                dangling_sha = store.repository.head()
-                changes_were_stored = dangling_sha != pre_work_sha
-                if changes_were_stored:
+                try:
+                    store.create_git_checkpoint(
+                        str(e),
+                        function=function,
+                        args=args,
+                        kwargs=kwargs,
+                        rollback=True
+                    )
+                    dangling_sha = store.repository.head()
+                    changes_were_stored = dangling_sha != pre_work_sha
+                    if changes_were_stored:
+                        logger.exception(
+                            "An error occurred that required rolling-back "
+                            "the git repository at %s from %s to %s.",
+                            store.local_path,
+                            dangling_sha,
+                            pre_work_sha,
+                        )
+                        store.log_error(
+                            "An error occurred while interacting with your task "
+                            "list, and your task list was recovered by "
+                            "rolling-back to the last known good state (%s).  "
+                            "Since your task list is synchronized with a taskd "
+                            "server, this will likely not have any negative "
+                            "effects.  Rollback ID: %s.",
+                            pre_work_sha,
+                            dangling_sha,
+                        )
+                        store.git_reset(pre_work_sha)
+                    else:
+                        logger.exception(
+                            "An error occured that did not require rolling-back "
+                            "the git repository at %s (at %s)",
+                            store.local_path,
+                            pre_work_sha
+                        )
+                except Exception:
                     logger.exception(
-                        "An error occurred that required rolling-back "
-                        "the git repository at %s from %s to %s.",
-                        store.local_path,
-                        dangling_sha,
-                        pre_work_sha,
+                        "An error occurred while rolling-back the repository."
                     )
-                    store.log_error(
-                        "An error occurred while interacting with your task "
-                        "list, and your task list was recovered by "
-                        "rolling-back to the last known good state (%s).  "
-                        "Since your task list is synchronized with a taskd "
-                        "server, this will likely not have any negative "
-                        "effects.  Rollback ID: %s.",
-                        pre_work_sha,
-                        dangling_sha,
-                    )
-                    store.git_reset(pre_work_sha)
-                else:
-                    logger.exception(
-                        "An error occured that did not require rolling-back "
-                        "the git repository at %s (at %s)",
-                        store.local_path,
-                        pre_work_sha
-                    )
-                raise
+                raise e
     except LockTimeout:
         lockfile_created = datetime.datetime.fromtimestamp(
             os.path.getctime(lockfile_path)
