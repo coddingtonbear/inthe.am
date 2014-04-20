26c1eca48fe62b0363eefc3c1faf80f2f69f7bce
Adam Coddington <me@adamcoddington.net>
2014-04-20 14:40:02 -0700

    Slightly simplifying upload process.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -1,6 +1,4 @@
-tar -czf /tmp/testing_artifacts.tar.gz --exclude '*.tar.gz'
+tar -czf /tmp/testing_artifacts.tar.gz --exclude '*.tar.gz' /tmp
 travis-artifacts upload --path /tmp/testing_artifacts.tar.gz
 tar -czf /tmp/task_data.tar.gz task_data
 travis-artifacts upload --path /tmp/task_data.tar.gz
-tar -czf /tmp/database.tar.gz db.sqlite3
-travis-artifacts upload --path /tmp/database.tar.gz
