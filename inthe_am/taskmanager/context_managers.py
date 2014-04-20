0ae791a3100f2eb87ffb43f32d4c7793bcd65a02
Adam Coddington <me@adamcoddington.net>
2014-04-20 03:05:45 -0700

    Putting things back in their rightful place.

--- inthe_am/taskmanager/context_managers.py
+++ inthe_am/taskmanager/context_managers.py
@@ -19,70 +19,67 @@ def git_checkpoint(
 ):
     lockfile_path = os.path.join(store.local_path, '.lock')
     pre_work_sha = None
-    logger.info(
-        "Creating git checkpoint on repository at %s",
-        store.local_path
-    )
     try:
         with PIDLockFile(lockfile_path, timeout=10):
-            #store.create_git_repository()
-            #try:
-            store.create_git_checkpoint(
-                message,
-                function=function,
-                args=args,
-                kwargs=kwargs,
-                pre_operation=True
-            )
-            yield
-            store.create_git_checkpoint(
-                message,
-                function=function,
-                args=args,
-                kwargs=kwargs
-            )
-            #except Exception as e:
-            #    store.create_git_checkpoint(
-            #        str(e),
-            #        function=function,
-            #        args=args,
-            #        kwargs=kwargs,
-            #        rollback=True
-            #    )
-            #    try:
-            #        dangling_sha = store.repository.head()
-            #    except KeyError:
-            #        dangling_sha = None
-            #    changes_were_stored = (
-            #        dangling_sha and dangling_sha != pre_work_sha
-            #    )
-            #    if changes_were_stored:
-            #        logger.exception(
-            #            "An error occurred that required rolling-back "
-            #            "the git repository at %s from %s to %s.",
-            #            store.local_path,
-            #            dangling_sha,
-            #            pre_work_sha,
-            #        )
-            #        store.log_error(
-            #            "An error occurred while interacting with your task "
-            #            "list, and your task list was recovered by "
-            #            "rolling-back to the last known good state (%s).  "
-            #            "Since your task list is synchronized with a taskd "
-            #            "server, this will likely not have any negative "
-            #            "effects.  Rollback ID: %s.",
-            #            pre_work_sha,
-            #            dangling_sha,
-            #        )
-            #        store.git_reset(pre_work_sha)
-            #    else:
-            #        logger.exception(
-            #            "An error occured that did not require rolling-back "
-            #            "the git repository at %s (at %s)",
-            #            store.local_path,
-            #            pre_work_sha
-            #        )
-            #    raise
+            store.create_git_repository()
+            try:
+                store.create_git_checkpoint(
+                    message,
+                    function=function,
+                    args=args,
+                    kwargs=kwargs,
+                    pre_operation=True
+                )
+                pre_work_sha = store.repository.head()
+                yield
+                store.create_git_checkpoint(
+                    message,
+                    function=function,
+                    args=args,
+                    kwargs=kwargs
+                )
+            except Exception as e:
+                store.create_git_checkpoint(
+                    str(e),
+                    function=function,
+                    args=args,
+                    kwargs=kwargs,
+                    rollback=True
+                )
+                try:
+                    dangling_sha = store.repository.head()
+                except KeyError:
+                    dangling_sha = None
+                changes_were_stored = (
+                    dangling_sha and dangling_sha != pre_work_sha
+                )
+                if changes_were_stored:
+                    logger.exception(
+                        "An error occurred that required rolling-back "
+                        "the git repository at %s from %s to %s.",
+                        store.local_path,
+                        dangling_sha,
+                        pre_work_sha,
+                    )
+                    store.log_error(
+                        "An error occurred while interacting with your task "
+                        "list, and your task list was recovered by "
+                        "rolling-back to the last known good state (%s).  "
+                        "Since your task list is synchronized with a taskd "
+                        "server, this will likely not have any negative "
+                        "effects.  Rollback ID: %s.",
+                        pre_work_sha,
+                        dangling_sha,
+                    )
+                    store.git_reset(pre_work_sha)
+                else:
+                    logger.exception(
+                        "An error occured that did not require rolling-back "
+                        "the git repository at %s (at %s)",
+                        store.local_path,
+                        pre_work_sha
+                    )
+                raise
     except LockTimeout:
         lockfile_created = datetime.datetime.fromtimestamp(
             os.path.getctime(lockfile_path)
