771f079d4454a871836e63e6d36c064bae6ae77e
Adam Coddington <me@adamcoddington.net>
2014-06-16 20:07:39 -0700

    Admin enhancements.

--- inthe_am/taskmanager/admin.py
+++ inthe_am/taskmanager/admin.py
@@ -4,18 +4,25 @@ from .models import TaskStore, TaskStoreActivityLog, UserMetadata
 
 
 class TaskStoreAdmin(admin.ModelAdmin):
-    search_fields = ('user__username', 'local_path', 'taskrc_extras', )
-    list_display = ('user', 'created', 'sync_enabled', 'twilio_enabled')
-    list_filter = ('configured', )
-
-    def twilio_enabled(self, obj):
-        return bool(obj.twilio_auth_token)
+    search_fields = (
+        'user__username', 'local_path', 'taskrc_extras',
+        'sms_whitelist', 'email_whitelist',
+    )
+    list_display = (
+        'user', 'created', 'last_synced',
+        'sync_enabled', 'pebble_cards_enabled', 'feed_enabled',
+    )
+    list_filter = (
+        'sync_enabled', 'pebble_cards_enabled', 'feed_enabled',
+    )
 
 admin.site.register(TaskStore, TaskStoreAdmin)
 
 
 class TaskStoreActivityLogAdmin(admin.ModelAdmin):
-    search_fields = ('store__user__username', 'message', )
+    search_fields = (
+        'store__user__username', 'message', 'store__local_path'
+    )
     list_display = (
         'username', 'last_seen', 'created', 'error', 'message', 'count'
     )
