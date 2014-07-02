a739a8a7e15688211436c153b01a9535539c3e70
Adam Coddington <me@adamcoddington.net>
2014-07-01 18:46:26 -0700

    Removing jquery.mousewheel and the hacks I had in place for translating mouse motions on non-OSX environments.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ var STATIC_FILES = [
   'jquery.datetimepicker.js',
   'jquery.growl.js',
   'jquery.touchwipe.min.js',
-  'jquery.mousewheel.min.js',
   'fastclick.js',
   'moment.min.js',
   'markdown.min.js',
