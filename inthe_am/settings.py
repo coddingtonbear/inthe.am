6dbdae7ea13ed02a6086ec3cb2220295b05262fa
Adam Coddington <me@adamcoddington.net>
2014-08-25 19:45:39 -0700

    Restoring a sane log-level for inthe_am.taskamanager.models.

--- inthe_am/settings.py
+++ inthe_am/settings.py
@@ -195,11 +195,6 @@ LOGGING = {
             'level': 'INFO',
             'propagate': True,
         },
-        'inthe_am.taskmanager.models': {
-            'handlers': ['store'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
         'inthe_am.taskmanager.taskwarrior_client': {
             'handlers': ['taskwarrior'],
             'level': 'DEBUG',
