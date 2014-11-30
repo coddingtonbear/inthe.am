8d40681520d60ba55aef9629d4343c5949d8c274
Adam Coddington <me@adamcoddington.net>
2014-11-29 23:38:19 -0800

    Slightly different method for setting setting file.

--- inthe_am/taskmanager/celery.py
+++ inthe_am/taskmanager/celery.py
@@ -1,13 +1,8 @@
 from __future__ import absolute_import
 
-import os
-
 from celery import Celery
 
 from django.conf import settings
-
-# set the default Django settings module for the 'celery' program.
-os.environ['DJANGO_SETTINGS_MODULE'] = 'inthe_am.celery_settings'
 
 app = Celery('inthe_am')
 
