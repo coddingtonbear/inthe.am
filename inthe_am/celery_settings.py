7f9a694bbabae412dbafdc69940b475fae52240f
Adam Coddington <me@adamcoddington.net>
2014-11-29 23:13:30 -0800

    Prevent double-imports from double-altering the filename.

--- inthe_am/celery_settings.py
+++ inthe_am/celery_settings.py
@@ -4,8 +4,9 @@ from .settings import *
 # Redirect celery logging elsewhere.
 for name, details in LOGGING['handlers'].items():
     if 'filename' in details:
-        details['filename'] = "".join([
-            os.path.splitext(details['filename'])[0],
-            '.celery',
-            os.path.splitext(details['filename'])[1]
-        ])
+        if 'celery' not in details['filename']:
+            details['filename'] = "".join([
+                os.path.splitext(details['filename'])[0],
+                '.celery',
+                os.path.splitext(details['filename'])[1]
+            ])
