de0aac33d175efaaf4e42d88b2a8863d1056a9c1
Adam Coddington <me@adamcoddington.net>
2014-11-29 18:01:18 -0800

    Force celery to log to separate files from the actual web process.

--- inthe_am/taskmanager/celery.py
+++ inthe_am/taskmanager/celery.py
@@ -1,10 +1,25 @@
 from __future__ import absolute_import
 
+import copy
+import logging
 import os
 
 from celery import Celery
 
 from django.conf import settings
+
+
+from inthe_am import settings as isettings
+logging_settings = copy.deepcopy(isettings.LOGGING)
+for name, details in logging_settings.items():
+    if 'filename' in details:
+        details['filename'] = "".join([
+            os.path.splitext(details['filename'])[0],
+            '.celery',
+            os.path.splitext(details['filename'])[1]
+        ])
+logging.config.dictConfig(logging_settings)
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inthe_am.settings')
