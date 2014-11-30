e7fe261639cf8121ac7bb41967e4918ce6e700fa
Adam Coddington <me@adamcoddington.net>
2014-11-29 23:48:59 -0800

    Load settings object directly.

--- inthe_am/taskmanager/celery.py
+++ inthe_am/taskmanager/celery.py
@@ -2,14 +2,14 @@ from __future__ import absolute_import
 
 from celery import Celery
 
-from django.conf import settings
+from inthe_am import celery_settings
 
 app = Celery('inthe_am')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object(celery_settings)
+app.autodiscover_tasks(lambda: celery_settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
