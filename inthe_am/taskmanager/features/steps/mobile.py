285cfb5d0beecefab53ef580f52380eaddcf6557
Adam Coddington <me@adamcoddington.net>
2014-12-27 01:03:46 -0600

    context.browser.driver; not just context.browser.

--- inthe_am/taskmanager/features/steps/mobile.py
+++ inthe_am/taskmanager/features/steps/mobile.py
@@ -3,11 +3,11 @@ from behave import given
 
 @given('the user is using a mobile device')
 def using_mobile_device(context):
-    context.browser.set_window_size(320, 700)
+    context.browser.driver.set_window_size(320, 700)
 
     if not hasattr(context, 'teardown_steps'):
         context.teardown_steps = []
 
     context.teardown_steps.append(
-        lambda context: context.browser.set_window_size(1024, 1024)
+        lambda context: context.browser.driver.set_window_size(1024, 1024)
     )
