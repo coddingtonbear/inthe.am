945ce5e17d8f51029f9440a3a7a49a4ce9b8827a
Adam Coddington <me@adamcoddington.net>
2015-01-17 10:33:24 -0800

    Updating documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Inthe.AM'
-copyright = u'2014, Adam Coddington'
+copyright = u'2015, Adam Coddington'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
