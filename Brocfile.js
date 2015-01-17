f4bca30d2b8364dea3de87ebdbf72fd87dae8d29
Adam Coddington <me@adamcoddington.net>
2015-01-16 17:06:22 -0800

    Unwelcome surprise: fingerprinted assets.

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,16 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+    fingerprint: {
+        exclude: [
+            'apple',
+            'colorschemes',
+            'static',
+            'logo.png',
+        ],
+    }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
