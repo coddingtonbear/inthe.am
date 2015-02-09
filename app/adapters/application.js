87ac30e86f7f87686fa4a3396723574899b9d00b
Adam Coddington <me@adamcoddington.net>
2015-02-08 23:25:55 -0800

    #152: Adding model and URL-calculation capabilities for kanban task instances.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,23 @@
 import DS from "ember-data";
 
+var KANBAN_ID_URL_RE = window.RegExp(".*kanban/([a-f0-9-]{36})");
+
 export default DS.DjangoTastypieAdapter.extend({
-    namespace: 'api/v1'
+    namespace: 'api/v1',
+    buildURL: function(type, id, record) {
+        var result = this._super(type, id, record);
+        if(type === "kanbanTask") {
+            var matched = KANBAN_ID_URL_RE.exec(window.location.href);
+            var board_id = Ember.KANBAN_BOARD_ID;
+            if (matched) {
+                board_id = matched[1];
+            }
+            result = "/api/v1/kanban/" + board_id + "/";
+            if (id) {
+                result = result + id + "/";
+            }
+            return result;
+        }
+        return result;
+    }
 });
---
+++ app/models/kanban-task.js
@@ -0,0 +1,5 @@
+import Task from "./task";
+
+var model = Task.extend({});
+
+export default model;
