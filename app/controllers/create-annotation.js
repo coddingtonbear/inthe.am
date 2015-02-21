874d7955ea5827f07d11621c66fdc1bcd0234b9c
Adam Coddington <me@adamcoddington.net>
2015-02-20 17:13:24 -0800

    Close the annotation form without submitting a new annotation if no value was supplied.

--- app/controllers/create-annotation.js
+++ app/controllers/create-annotation.js
@@ -10,11 +10,19 @@ var controller = Ember.ObjectController.extend({
             var annotations = model.get('annotations');
             var field = $("#new_annotation_body");
             var form = $("#new_annotation_form");
+            var value = field.val();
 
             if (annotations === null) {
                 annotations = [];
             }
-            annotations.pushObject(field.val());
+            if (!value) {
+                // If they didn't enter any text into the annotation
+                // form, just close the dialog and go about your day.
+                form.foundation('reveal', 'close');
+                return;
+            }
+
+            annotations.pushObject(value);
             model.set('annotations', annotations);
             application.showLoading();
             model.save().then(function(){
