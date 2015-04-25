32e66db18fccf4a04c01efd4828fc678b446ce53
Adam Coddington <me@adamcoddington.net>
2015-04-25 14:39:22 -0700

    Write ember-cli's stdout to the console so we can tell why it didn't start when it fails.

--- inthe_am/taskmanager/management/commands/runtests.py
+++ inthe_am/taskmanager/management/commands/runtests.py
@@ -3,6 +3,7 @@ from optparse import make_option
 import socket
 import subprocess
 import sys
+import tempfile
 import threading
 import time
 
@@ -35,40 +36,41 @@ class Command(RunserverCommand):
         return subprocess.call(command, env=env)
 
     def handle(self, *args, **kwargs):
-        fnull = open(os.devnull, 'w')
+        with tempfile.NamedTemporaryFile(prefix='ember', delete=False) as out:
+            ember = threading.Thread(
+                target=self.run_ember,
+                kwargs={
+                    'stdout': out,
+                    'stderr': out
+                }
+            )
+            ember.daemon = True
+            ember.start()
 
-        ember = threading.Thread(
-            target=self.run_ember,
-            kwargs={
-                'stdout': fnull,
-                'stderr': subprocess.STDOUT
-            }
-        )
-        ember.daemon = True
-        ember.start()
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            started = time.time()
+            connected = False
+            while time.time() < started + 60:
+                result = s.connect_ex(('127.0.0.1', 8001, ))
+                if result == 0:
+                    s.close()
+                    connected = True
+                    break
+                time.sleep(1)
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        started = time.time()
-        connected = False
-        while time.time() < started + 60:
-            result = s.connect_ex(('127.0.0.1', 8000, ))
-            if result == 0:
-                s.close()
-                connected = True
-                break
-            time.sleep(1)
+            if not connected:
+                print "Ember server did not start!"
+                out.seek(0)
+                print out.read()
+                sys.exit(1)
 
-        if not connected:
-            print "Ember server did not start!"
-            sys.exit(1)
+            test_args = []
+            if kwargs['wip']:
+                test_args.append('--behave_wip')
 
-        test_args = []
-        if kwargs['wip']:
-            test_args.append('--behave_wip')
-
-        try:
-            result = self.run_tests(*test_args)
-        except:
-            result = 1
+            try:
+                result = self.run_tests(*test_args)
+            except:
+                result = 1
         self.teardown()
         sys.exit(result)
