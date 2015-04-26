66fe2774c98a8112c185266792f779c820bfa6f0
Adam Coddington <me@adamcoddington.net>
2015-04-26 00:50:01 -0700

    Let's try a different port.

--- inthe_am/taskmanager/management/commands/runtests.py
+++ inthe_am/taskmanager/management/commands/runtests.py
@@ -23,7 +23,7 @@ class Command(RunserverCommand):
 
     def run_tests(self, *args):
         env = os.environ.copy()
-        env['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8009'
+        env['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8007'
 
         command = [
             'python',
