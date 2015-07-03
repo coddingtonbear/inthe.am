b55c4cf87580f534f2a579135b5332ae44b85244
Adam Coddington <me@adamcoddington.net>
2015-07-03 16:18:36 -0700

    Instruct travis to upload the debs for task and taskd, too.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -3,3 +3,5 @@ tar -czf /tmp/testing_artifacts.tar.gz --exclude '*.tar.gz' /tmp
 travis-artifacts upload --path /tmp/testing_artifacts.tar.gz
 tar -czf /tmp/task_data.tar.gz task_data
 travis-artifacts upload --path /tmp/task_data.tar.gz
+travis-artifacts upload --path /tmp/task_2.3.0-1_i386.deb
+travis-artifacts upload --path /tmp/taskd_1.0.0-1_i386.deb
