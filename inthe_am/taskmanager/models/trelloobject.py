e8515ab45c2122c19fbe222f7328c7b38d197448
Adam Coddington <me@adamcoddington.net>
2015-07-05 15:20:31 -0700

    Fixing a bug in which we'd improperly try adding color tags.

--- inthe_am/taskmanager/models/trelloobject.py
+++ inthe_am/taskmanager/models/trelloobject.py
@@ -192,13 +192,13 @@ class TrelloObject(models.Model):
                 if l.get('color')
             ])
             task_tags = set(task.get('tags'))
-            tags_to_add = ','.join(
+            tags_to_add = (
                 (existing_labels - task_tags) & possible_colors
             )
             if tags_to_add:
-                self.client.new_label(self.id, tags_to_add)
+                self.client.new_label(self.id, ','.join(tags_to_add))
 
-            tags_to_delete = ','.join(
+            tags_to_delete = (
                 (task_tags - existing_labels) & possible_colors
             )
             for tag_to_delete in tags_to_delete:
