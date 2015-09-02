ca968adb1c30210f616f659a9cc6fff12c1e94cc
Adam Coddington <me@adamcoddington.net>
2015-09-01 20:55:47 -0700

    Enabling django-silk.

--- inthe_am/settings.py
+++ inthe_am/settings.py
@@ -59,9 +59,11 @@ INSTALLED_APPS = (
     'raven.contrib.django.raven_compat',
     'django_behave',
     'storages',
+    'silk',
 )
 
 MIDDLEWARE_CLASSES = (
+    'silk.middleware.SilkyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -368,6 +370,10 @@ ANNOUNCEMENTS_CHANNEL = '__general__'
 
 # Streaming ticket updates enabled?
 STREAMING_UPDATES_ENABLED = True
+
+SILKY_PYTHON_PROFILER = True
+SILKY_AUTHENTICATION = True  # User must login
+SILKY_AUTHORISATION = True  # User must have permissions
 
 # Must be sourced from environment:
 #  SOCIAL_AUTH_GOOGLE_OAUTH2_KEY
