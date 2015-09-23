d210a8624a6490c87719df98dc547481c89ca7b1
Adam Coddington <me@adamcoddington.net>
2015-09-22 23:52:15 -0700

    Fixing paths for newrelic admin.

--- start_celery.sh
+++ start_celery.sh
@@ -3,4 +3,4 @@ set -e
 source /var/www/envs/twweb/bin/activate
 export DJANGO_SETTINGS_MODULE=inthe_am.celery_settings
 export NEW_RELIC_CONFIG_FILE=/var/www/twweb/newrelic-celery.ini
-exec /var/www/envs/bin/newrelic-admin run-program /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
+exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
