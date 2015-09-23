7ec21a7492db2a40d7757a4ef53f19e5d75d53a3
Adam Coddington <me@adamcoddington.net>
2015-09-22 23:44:08 -0700

    Run main app via Newrelic.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/python /var/www/twweb/manage.py run_gunicorn --config /var/www/twweb/gunicorn.conf.py
+exec /var/www/envs/bin/newrelic-admin run-program /var/www/envs/twweb/bin/python /var/www/twweb/manage.py run_gunicorn --config /var/www/twweb/gunicorn.conf.py
