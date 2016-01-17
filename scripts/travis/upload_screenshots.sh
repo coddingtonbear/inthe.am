315060f1a6ce9fddae3025f8f9b5384f462696c3
Adam Coddington <me@adamcoddington.net>
2016-01-16 21:18:38 -0800

    Extra power.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -1,11 +1,11 @@
 travis-artifacts upload --path /tmp/task_2.3.0-1*.deb
 travis-artifacts upload --path /tmp/taskd_1.0.0-1*.deb
-rm /tmp/*.deb
-rm -rf /tmp/pip_cache
-rm -rf /tmp/npm-*
-rm -rf /tmp/async-disk-cache
-rm -rf /tmp/root
-rm -rf /tmp/twweb
+sudo rm /tmp/*.deb
+sudo rm -rf /tmp/pip_cache
+sudo rm -rf /tmp/npm-*
+sudo rm -rf /tmp/async-disk-cache
+sudo rm -rf /tmp/root
+sudo rm -rf /tmp/twweb
 tar -czf /tmp/testing_artifacts.tar.gz --exclude '*.tar.gz' /tmp
 travis-artifacts upload --path /tmp/testing_artifacts.tar.gz
 tar -czf /tmp/task_data.tar.gz task_data
