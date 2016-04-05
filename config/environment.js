d807be4c19b485d72de04a5b1e124c3f1a2e19d7
Adam Coddington <me@adamcoddington.net>
2016-04-04 20:12:08 -0700

    Fixing oversights.

--- config/environment.js
+++ config/environment.js
@@ -42,7 +42,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.API_HOST = 'http://inthe.am/';
+    ENV.APP.API_HOST = 'https://inthe.am/';
   }
 
   return ENV;
