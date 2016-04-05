12571609757ea7f13d0a025def9b2d2141f143d6
Adam Coddington <me@adamcoddington.net>
2016-04-04 21:09:01 -0700

    Do not use newrelic with status handler.

--- start_status.sh
+++ start_status.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-export EVENTLET=true
-export NEW_RELIC_CONFIG_FILE=/var/www/twweb/newrelic-status.ini
-exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/gunicorn inthe_am.wsgi --config /var/www/twweb/gunicorn_status.conf.py
+exec /var/www/envs/twweb/bin/gunicorn inthe_am.wsgi --config /var/www/twweb/gunicorn_status.conf.py
