98e7f08acf1b6d87da6b255dafc6c1c69e33e30b
Adam Coddington <me@adamcoddington.net>
2016-04-05 12:52:57 -0700

    Switch from gunicorn to uwsgi.

--- gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-user = 'www-data'
-group = 'www-data'
-logfile = '/var/www/twweb/logs/gunicorn.log'
-workers = 3
-loglevel = 'info'
-bind = '127.0.0.1:8040'
-timeout = 15
-name = 'inthe_am'
-preload = True
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/gunicorn inthe_am.wsgi --config /var/www/twweb/gunicorn.conf.py
+exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/uwsgi --ini /var/www/twweb/uwsgi.ini
