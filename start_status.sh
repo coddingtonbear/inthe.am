605fac0420a41cef3790a60359fe95a2c60a5496
Adam Coddington <me@adamcoddington.net>
2016-04-04 20:00:38 -0700

    Updating application launcher to use modern gunicorn launcher.

--- start_status.sh
+++ start_status.sh
@@ -3,4 +3,4 @@ set -e
 source /var/www/envs/twweb/bin/activate
 export EVENTLET=true
 export NEW_RELIC_CONFIG_FILE=/var/www/twweb/newrelic-status.ini
-exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/python /var/www/twweb/manage.py run_gunicorn -k eventlet --config /var/www/twweb/gunicorn_status.conf.py
+exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/gunicorn inthe_am.wsgi -k eventlet --config /var/www/twweb/gunicorn_status.conf.py
