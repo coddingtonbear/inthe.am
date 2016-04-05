28a452b5c8ce1ceaefc19633d749a07994cc91ef
Adam Coddington <me@adamcoddington.net>
2016-04-04 21:16:55 -0700

    Try separating out the wsgi files between main and status.

--- start_status.sh
+++ start_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/gunicorn inthe_am.wsgi --config /var/www/twweb/gunicorn_status.conf.py
+exec /var/www/envs/twweb/bin/gunicorn inthe_am.wsgi_status --config /var/www/twweb/gunicorn_status.conf.py
