f931b5ee3ed6c660c3566e4c392746bcd574d28d
Adam Coddington <me@adamcoddington.net>
2016-04-06 12:37:04 -0700

    Adding celery flower for easier monitoring of tasks.

---
+++ start_celery_flower.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+source /var/www/envs/twweb/bin/activate
+export DJANGO_SETTINGS_MODULE=inthe_am.celery_settings
+exec /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker --port=5555
