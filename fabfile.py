aa6c76c6db60769ccd8c8d75e9f0c840f3aee652
Adam Coddington <me@adamcoddington.net>
2016-04-10 15:13:12 -0700

    Restart celery at outset if building ember templates.

--- fabfile.py
+++ fabfile.py
@@ -22,6 +22,10 @@ def deploy(install='yes', build='yes', chown='no'):
     local('git checkout development')
     if chown == 'yes':
         sudo('/bin/chown -R www-data:www-data /var/www/twweb', shell=False)
+    if build == 'yes':
+        # Restart celery in case it's using a lot of memory -- which is
+        # totally a possibility.
+        sudo('/usr/sbin/service twweb-celery restart', shell=False)
     with cd('/var/www/twweb'):
         run('git fetch origin')
         run('git merge origin/master')
@@ -30,9 +34,6 @@ def deploy(install='yes', build='yes', chown='no'):
             run('npm install')
             virtualenv('pip install -r /var/www/twweb/requirements-frozen.txt')
         if build == 'yes':
-            # Restart celery in case it's using a lot of memory -- which is
-            # totally a possibility.
-            sudo('/usr/sbin/service twweb-celery restart', shell=False)
             # Clear out vendor sourcemaps
             with settings(warn_only=True):
                 run(
