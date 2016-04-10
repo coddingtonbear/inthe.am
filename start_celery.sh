bd5f879896dc178aef5445b43d06c5d8de100c1d
Adam Coddington <me@adamcoddington.net>
2016-04-10 13:34:13 -0700

    Updating celery configuration to properly handle not having a newrelic configuration (in development).

--- start_celery.sh
+++ start_celery.sh
@@ -3,4 +3,8 @@ set -e
 source /var/www/envs/twweb/bin/activate
 export DJANGO_SETTINGS_MODULE=inthe_am.celery_settings
 export NEW_RELIC_CONFIG_FILE=/var/www/twweb/newrelic-celery.ini
-exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
+if [ -f $NEW_RELIC_CONFIG_FILE ]; then
+    exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
+else
+    exec /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
+fi
