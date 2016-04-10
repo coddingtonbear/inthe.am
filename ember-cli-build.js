37ab789ea2ca24df580a3a5da87a6b8b5de6ef82
Adam Coddington <me@adamcoddington.net>
2016-04-09 20:43:05 -0700

    Enable sourcemaps.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,6 +2,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
+      sassOptions: {
+          sourceMap: true,
+      },
       fingerprint: {
           exclude: [
               'apple',
