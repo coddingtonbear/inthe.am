b4fe4f79d6fb7ead93e84acbd5e21c0112acb6d1
Adam Coddington <me@adamcoddington.net>
2017-05-20 15:09:44 -0700

    Adding migrations adding statistics.

---
+++ inthe_am/taskmanager/migrations/0004_taskstorestatistic.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taskmanager', '0003_resthook'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TaskStoreStatistic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('measure', models.CharField(max_length=50, choices=[(b'size', b'Repository Size')])),
+                ('value', models.BigIntegerField()),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('store', models.ForeignKey(related_name='statistics', to='taskmanager.TaskStore')),
+            ],
+        ),
+    ]
