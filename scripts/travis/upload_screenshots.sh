1edda33d1c1491d120ea79383c8dbd90c8e325c4
Adam Coddington <me@adamcoddington.net>
2017-08-30 11:00:57 -0700

    Removing now-obsolete 'travis-artifacts' calls.

--- scripts/travis/upload_screenshots.sh
+++ scripts/travis/upload_screenshots.sh
@@ -1,5 +1,3 @@
-travis-artifacts upload --path /tmp/task_2.3.0-1*.deb
-travis-artifacts upload --path /tmp/taskd_1.0.0-1*.deb
 sudo rm /tmp/*.deb
 sudo rm -rf /tmp/pip_cache
 sudo rm -rf /tmp/npm-*
@@ -7,6 +5,4 @@ sudo rm -rf /tmp/async-disk-cache
 sudo rm -rf /tmp/root
 sudo rm -rf /tmp/twweb
 tar -czf /tmp/testing_artifacts.tar.gz --exclude '*.tar.gz' /tmp
-travis-artifacts upload --path /tmp/testing_artifacts.tar.gz
 tar -czf /tmp/task_data.tar.gz task_data
-travis-artifacts upload --path /tmp/task_data.tar.gz
