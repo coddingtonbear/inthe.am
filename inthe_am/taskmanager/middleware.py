031d5d196828bd62da837c61299c583b87939608
Adam Coddington <me@adamcoddington.net>
2017-11-14 20:39:06 -0800

    [#276] Do not assume a user will be available.

--- inthe_am/taskmanager/middleware.py
+++ inthe_am/taskmanager/middleware.py
@@ -3,7 +3,7 @@ from inthe_am.taskmanager.models import TaskStore
 
 class AuthenticationTokenMiddleware(object):
     def process_response(self, request, response):
-        if request.user.is_authenticated():
+        if hasattr(request, 'user') and request.user.is_authenticated():
             store = TaskStore.get_for_user(request.user)
             response.set_cookie('authentication_token', store.api_key.key)
         else:
