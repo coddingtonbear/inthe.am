a66b08aacabd1b24a401729854e5074a2c98f0d0
Adam Coddington <me@adamcoddington.net>
2018-08-28 12:50:32 -0700

    Minor typo.

--- start_status.sh
+++ start_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/uwsgi --gevent 25 --ini /var/www/twweb/uwsgi_status.ini --status /tmp/twweb_status.socket
+exec /var/www/envs/twweb/bin/uwsgi --gevent 25 --ini /var/www/twweb/uwsgi_status.ini --stats /tmp/twweb_status.socket
