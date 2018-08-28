e97b0f1f298df298025c6f73257205ec9c45e8cf
Adam Coddington <me@adamcoddington.net>
2018-08-28 12:47:09 -0700

    Adding support for exporting uwsgi statistics for troubleshooting.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/uwsgi --ini /var/www/twweb/uwsgi.ini
+exec /var/www/envs/twweb/bin/uwsgi --ini /var/www/twweb/uwsgi.ini --stats /tmp/twweb.socket
--- start_status.sh
+++ start_status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/uwsgi --gevent 50 --ini /var/www/twweb/uwsgi_status.ini
+exec /var/www/envs/twweb/bin/uwsgi --gevent 25 --ini /var/www/twweb/uwsgi_status.ini --status /tmp/twweb_status.socket
