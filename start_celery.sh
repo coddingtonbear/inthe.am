1d277fcaa32dd78e83ca39c4db19adbf98242e0b
Adam Coddington <me@adamcoddington.net>
2019-10-04 06:59:40 -0700

    cd into the appropriate directory for finding the settings module for celery.

--- start_celery.sh
+++ start_celery.sh
@@ -4,6 +4,7 @@ if [ ! -d /mnt/task_data/task_data ]; then
     echo "Task data is not mounted!"
     exit 1
 fi
+cd /var/www/twweb
 source /var/www/envs/twweb/bin/activate
 export DJANGO_SETTINGS_MODULE=inthe_am.celery_settings
 exec /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
