336242002aad69cc29295483e727551ab968b9fc
Adam Coddington <me@adamcoddington.net>
2019-10-04 06:53:16 -0700

    Removing newrelic configuration for celery.

--- start_celery.sh
+++ start_celery.sh
@@ -6,9 +6,4 @@ if [ ! -d /mnt/task_data/task_data ]; then
 fi
 source /var/www/envs/twweb/bin/activate
 export DJANGO_SETTINGS_MODULE=inthe_am.celery_settings
-export NEW_RELIC_CONFIG_FILE=/var/www/twweb/newrelic-celery.ini
-if [ -f $NEW_RELIC_CONFIG_FILE ]; then
-    exec /var/www/envs/twweb/bin/newrelic-admin run-program /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
-else
-    exec /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
-fi
+exec /var/www/envs/twweb/bin/celery -A inthe_am.taskmanager.celery worker -l info
