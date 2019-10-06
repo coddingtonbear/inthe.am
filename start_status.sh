606109278de7e18f1c2a28c154dd2ee19a186363
Adam Coddington <me@adamcoddington.net>
2019-10-05 18:44:04 -0700

    Remove duplicate gevent setting from cli.

--- start_status.sh
+++ start_status.sh
@@ -6,4 +6,4 @@ if [ ! -d /mnt/task_data/task_data ]; then
 fi
 source /var/www/envs/twweb/bin/activate
 export DJANGO_SETTINGS_MODULE=inthe_am.settings
-exec /var/www/envs/twweb/bin/uwsgi --gevent 25 --ini /var/www/twweb/uwsgi_status.ini --stats /tmp/twweb_status.socket
+exec /var/www/envs/twweb/bin/uwsgi --ini /var/www/twweb/uwsgi_status.ini --stats /tmp/twweb_status.socket
