332f68f1f2bb1fe8e3815ebff84cee3b4f9f15db
Adam Coddington <me@adamcoddington.net>
2020-10-15 21:13:21 -0700

    Deleting some obsolete things.

--- start_log_consumer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-if [ ! -d /mnt/task_data/task_data ]; then
-    echo "Task data is not mounted!"
-    exit 1
-fi
-source /var/www/envs/twweb/bin/activate
-exec /var/www/envs/twweb/bin/python /var/www/twweb/manage.py log_consumer /var/taskd/taskd.log
--- start_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-if [ ! -d /mnt/task_data/task_data ]; then
-    echo "Task data is not mounted!"
-    exit 1
-fi
-source /var/www/envs/twweb/bin/activate
-export DJANGO_SETTINGS_MODULE=inthe_am.settings
-exec /var/www/envs/twweb/bin/uwsgi --ini /var/www/twweb/uwsgi_status.ini --stats /tmp/twweb_status.socket
