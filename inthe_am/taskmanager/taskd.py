ad52e3e5f9cf4e35838753b4f74f6574f2ff363e
Adam Coddington <me@adamcoddington.net>
2020-10-16 17:50:01 -0700

    Convert server-level requests to classmethods; allow specifying request kwargs.

--- inthe_am/taskmanager/taskd.py
+++ inthe_am/taskmanager/taskd.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 from typing_extensions import Literal
 
 import requests
@@ -20,24 +21,32 @@ class TaskdAccountManager:
 
         super().__init__()
 
+    @classmethod
     def _make_request(
-        self, method: HttpMethod, path: str = "", raise_for_error=True, data: str = ""
+        cls,
+        method: HttpMethod,
+        path: str = "",
+        raise_for_error=True,
+        data: str = "",
+        **request_kwargs: Any,
     ) -> requests.models.Response:
         response = requests.request(
-            method, f"http://{settings.TASKD_HTTP}/{path}", data=data,
+            method, f"http://{settings.TASKD_HTTP}/{path}", data=data, **request_kwargs
         )
         if raise_for_error and not response.ok:
             raise TaskdAccountManagementError(response)
         return response
 
-    def get_ca_cert(self) -> str:
-        return self._make_request("get").json()["ca_cert"]
+    @classmethod
+    def get_ca_cert(cls, **request_kwargs: Any) -> str:
+        return cls._make_request("get", **request_kwargs).json()["ca_cert"]
 
-    def get_signing_template(self) -> str:
-        return self._make_request("get").json()["signing_template"]
+    @classmethod
+    def get_signing_template(cls, **request_kwargs: Any) -> str:
+        return cls._make_request("get", **request_kwargs).json()["signing_template"]
 
-    def get_credentials(self) -> str:
-        return self.make_user_request("get").json()["credentials"]
+    def get_credentials(self, **request_kwargs: Any) -> str:
+        return self.make_user_request("get", **request_kwargs).json()["credentials"]
 
     def make_user_request(
         self,
@@ -45,41 +54,59 @@ class TaskdAccountManager:
         path: str = "",
         raise_for_error: bool = True,
         data: str = "",
+        **request_kwargs: Any,
     ) -> requests.models.Response:
         final_path = f"{self._org_name}/{self._user_name}/"
         if path:
             final_path = final_path + path + "/"
 
         return self._make_request(
-            method, final_path, raise_for_error=raise_for_error, data=data,
+            method,
+            final_path,
+            raise_for_error=raise_for_error,
+            data=data,
+            **request_kwargs,
         )
 
-    def get_new_certificate(self, csr: str, label: str = "") -> str:
+    def get_new_certificate(
+        self, csr: str, label: str = "", **request_kwargs: Any
+    ) -> str:
         return self.make_user_request(
-            "post", "certificates", data=json.dumps({"csr": csr, "label": label,})
+            "post",
+            "certificates",
+            data=json.dumps({"csr": csr, "label": label}, **request_kwargs),
         ).json()["certificate"]
 
-    def create(self) -> None:
-        self.make_user_request("put", data=json.dumps({}))
+    def create(self, **request_kwargs: Any) -> None:
+        self.make_user_request("put", data=json.dumps({}), **request_kwargs)
 
-    def suspend(self) -> None:
-        self.make_user_request("put", data=json.dumps({"is_suspended": True}))
+    def suspend(self, **request_kwargs: Any) -> None:
+        self.make_user_request(
+            "put", data=json.dumps({"is_suspended": True}), **request_kwargs
+        )
 
-    def resume(self) -> None:
-        self.make_user_request("put", data=json.dumps({"is_suspended": False}))
+    def resume(self, **request_kwargs: Any) -> None:
+        self.make_user_request(
+            "put", data=json.dumps({"is_suspended": False}), **request_kwargs
+        )
 
-    def delete(self) -> None:
-        self.make_user_request("delete")
+    def delete(self, **request_kwargs: Any) -> None:
+        self.make_user_request("delete", **request_kwargs)
 
-    def exists(self) -> bool:
-        return self.make_user_request("get", raise_for_error=False).status_code == 200
+    def exists(self, **request_kwargs: Any) -> bool:
+        return (
+            self.make_user_request(
+                "get", raise_for_error=False, **request_kwargs
+            ).status_code
+            == 200
+        )
 
-    def get_data(self) -> bytes:
-        response = self.make_user_request("get", "data")
+    def get_data(self, **request_kwargs: Any) -> bytes:
+        response = self.make_user_request("get", "data", **request_kwargs)
         return response.content
 
-    def delete_data(self) -> None:
+    def delete_data(self, **request_kwargs: Any) -> None:
         try:
-            self.make_user_request("delete", "data")
+            self.make_user_request("delete", "data", **request_kwargs)
         except TaskdAccountManagementError:
             pass
