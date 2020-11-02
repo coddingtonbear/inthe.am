53b6fc8d98560c261612b7558fb00b04ec3803f3
Adam Coddington <me@adamcoddington.net>
2020-11-01 16:36:45 -0800

    Adding task list.

---
+++ ui/src/components/TaskListItem.tsx
@@ -0,0 +1,146 @@
+import React, {FunctionComponent} from 'react'
+import classnames from 'classnames'
+import {DateTime, Duration} from 'luxon'
+
+import {Task} from '../clients/tasks'
+
+import Icon from './Icon'
+
+export type TaskClass =
+  | 'active'
+  | 'blocking'
+  | 'blocked'
+  | 'overdue'
+  | 'due__today'
+  | 'due'
+  | 'recurring'
+  | 'pri__H'
+  | 'pri__M'
+  | 'pri__L'
+  | 'tagged'
+  | ''
+
+export const taskIsRelevant = (task: Task): boolean => {
+  return !['completed', 'deleted'].includes(task.status)
+}
+
+export const taskIsBlocking = (tasks: Task[], task: Task): boolean => {
+  const blockedTasks = tasks.filter(
+    (otherTask) => task.blocks && task.blocks.includes(otherTask.uuid)
+  )
+  for (const otherTask of blockedTasks) {
+    if (taskIsRelevant(otherTask)) {
+      return true
+    }
+  }
+  return false
+}
+
+export const taskIsBlocked = (tasks: Task[], task: Task): boolean => {
+  for (const otherTask of tasks) {
+    if (
+      otherTask.blocks &&
+      otherTask.blocks.includes(task.uuid) &&
+      taskIsRelevant(otherTask)
+    ) {
+      return true
+    }
+  }
+  return false
+}
+
+export const getTaskwarriorClass = (tasks: Task[], task: Task): TaskClass => {
+  if (task.start) {
+    return 'active'
+  } else if (taskIsBlocking(tasks, task)) {
+    return 'blocking'
+  } else if (taskIsBlocked(tasks, task)) {
+    return 'blocked'
+  } else if (task.due && DateTime.fromISO(task.due) < DateTime.local()) {
+    return 'overdue'
+  } else if (
+    task.due &&
+    DateTime.fromISO(task.due).hasSame(DateTime.local(), 'day')
+  ) {
+    return 'due__today'
+  } else if (
+    task.due &&
+    DateTime.fromISO(task.due) <
+      DateTime.local().plus(Duration.fromObject({days: 7}))
+  ) {
+    return 'due'
+  } else if (task.imask) {
+    return 'recurring'
+  } else if (task.priority === 'H') {
+    return 'pri__H'
+  } else if (task.priority === 'M') {
+    return 'pri__M'
+  } else if (task.priority === 'L') {
+    return 'pri__L'
+  } else if (task.tags && task.tags.length) {
+    return 'tagged'
+  }
+
+  return ''
+}
+
+export const getTaskIcon = (task: Task): string => {
+  if (task.status === 'completed') {
+    return 'check'
+  } else if (task.start) {
+    return 'asterisk'
+  } else if (task.due) {
+    return 'clock'
+  } else {
+    return 'target'
+  }
+}
+
+export interface Props {
+  tasks: Task[]
+  task: Task
+}
+
+const TaskListItem: FunctionComponent<Props> = ({tasks, task}) => {
+  return (
+    <div className="task">
+      <div
+        className={classnames('task-item', getTaskwarriorClass(tasks, task))}
+      >
+        <div className={classnames('task-list-icon', task.status)}>
+          <Icon name={getTaskIcon(task)} />
+        </div>
+        <div className="task-list-item">
+          {task.project && (
+            <h5 className="status" title={task.project}>
+              {task.project}
+            </h5>
+          )}
+          <p className="description">{task.description}</p>
+          <p className="tags">
+            {task.annotations && <Icon name="page-edit" />}
+            {task.tags &&
+              task.tags.map((tag) => {
+                return (
+                  <React.Fragment key={tag}>
+                    <Icon name="price-tag" />
+                    {tag}
+                  </React.Fragment>
+                )
+              })}
+          </p>
+          <p className="duedatetime">
+            {task.due && (
+              <>
+                <Icon name="clock" />
+                {task.due}
+              </>
+            )}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TaskListItem
--- ui/src/components/Tasks.tsx
+++ ui/src/components/Tasks.tsx
@@ -1,11 +1,47 @@
 import React, {FunctionComponent} from 'react'
 import {useSelector} from 'react-redux'
 
-import {RootState} from '../store'
+import {RootState, useAppDispatch} from '../store'
+import {refreshTasks} from '../reducers/tasks'
+import TaskListItem from './TaskListItem'
 
 const Tasks: FunctionComponent = () => {
   const tasks = useSelector((state: RootState) => state.tasks)
-  return <div>TASKS</div>
+  const dispatch = useAppDispatch()
+  const stylesheet = useSelector((state: RootState) =>
+    state.status.logged_in ? state.status.colorscheme : null
+  )
+
+  React.useEffect(() => {
+    const stylesheetId = 'colorscheme-stylesheet'
+
+    if (stylesheet) {
+      const existing = document.getElementById(stylesheetId)
+      if (existing) {
+        existing.remove()
+      }
+
+      const sheet = document.createElement('link')
+      sheet.rel = 'stylesheet'
+      sheet.href = `/assets/colorschemes/${stylesheet}.css`
+      sheet.id = stylesheetId
+      document.head.appendChild(sheet)
+    }
+  }, [stylesheet])
+
+  React.useEffect(() => {
+    dispatch(refreshTasks())
+  }, [])
+
+  return (
+    <div className="row full-width">
+      <div id="list" className="task-list">
+        {tasks.map((task) => (
+          <TaskListItem tasks={tasks} task={task} key={task.id} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Tasks
