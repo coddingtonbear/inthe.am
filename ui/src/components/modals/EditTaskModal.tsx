9de8167ba3114b7421d1070f4c080d41c2da0b58
Adam Coddington <me@adamcoddington.net>
2020-11-08 12:35:07 -0800

    Adding support for setting due/wait/scheduled dates using react-datepicker.

--- ui/src/components/modals/EditTaskModal.tsx
+++ ui/src/components/modals/EditTaskModal.tsx
@@ -1,11 +1,15 @@
 import React, {FunctionComponent} from 'react'
 import {useSelector} from 'react-redux'
-import {GridContainer, Grid, Cell} from 'react-foundation'
+import {Grid, Cell} from 'react-foundation'
+import DatePicker from 'react-datepicker'
+import {DateTime} from 'luxon'
 
 import {RootState, useAppDispatch} from '../../store'
 import {taskActions, editTaskModalActions} from '../../reducers'
 import {commitTask, createTask} from '../../thunks/tasks'
 import {TaskUpdate} from '../../clients/tasks'
+
+import 'react-datepicker/dist/react-datepicker.css'
 
 const EditTaskModal: FunctionComponent = () => {
   const selectedTask = useSelector(
@@ -15,19 +19,26 @@ const EditTaskModal: FunctionComponent = () => {
   const [description, setDescription] = React.useState<string>('')
   const [project, setProject] = React.useState<string>('')
   const [tags, setTags] = React.useState<string[]>([])
-  const [due, setDue] = React.useState<string | undefined>(undefined)
-  const [wait, setWait] = React.useState<string | undefined>(undefined)
-  const [scheduled, setScheduled] = React.useState<string | undefined>(
-    undefined
-  )
+  const [due, setDue] = React.useState<Date | undefined>(undefined)
+  const [wait, setWait] = React.useState<Date | undefined>(undefined)
+  const [scheduled, setScheduled] = React.useState<Date | undefined>(undefined)
 
   React.useEffect(() => {
     setDescription(selectedTask?.description ?? '')
     setProject(selectedTask?.project ?? '')
     setTags(selectedTask?.tags ?? [])
-    setDue(selectedTask?.due ?? undefined)
-    setWait(selectedTask?.wait ?? undefined)
-    setScheduled(selectedTask?.scheduled ?? undefined)
+    setDue(undefined)
+    setWait(undefined)
+    setScheduled(undefined)
+    if (selectedTask?.due) {
+      setDue(DateTime.fromISO(selectedTask.due).toJSDate())
+    }
+    if (selectedTask?.wait) {
+      setWait(DateTime.fromISO(selectedTask.wait).toJSDate())
+    }
+    if (selectedTask?.scheduled) {
+      setScheduled(DateTime.fromISO(selectedTask.scheduled).toJSDate())
+    }
   }, [JSON.stringify(selectedTask)])
 
   const dispatch = useAppDispatch()
@@ -41,9 +52,15 @@ const EditTaskModal: FunctionComponent = () => {
       description,
       project,
       tags,
-      due,
-      wait,
-      scheduled,
+    }
+    if (due) {
+      partialTask.due = DateTime.fromJSDate(due).toISO()
+    }
+    if (wait) {
+      partialTask.wait = DateTime.fromJSDate(wait).toISO()
+    }
+    if (scheduled) {
+      partialTask.scheduled = DateTime.fromJSDate(scheduled).toISO()
     }
 
     if (selectedTask.uuid) {
@@ -62,6 +79,18 @@ const EditTaskModal: FunctionComponent = () => {
 
   function onCancelEdit() {
     dispatch(editTaskModalActions.unselectTaskForEdit())
+  }
+
+  function getSingleDateOrUndefined(
+    maybeDate: Date | [Date, Date] | null
+  ): Date | undefined {
+    if (maybeDate instanceof Date) {
+      return maybeDate
+    }
+    if (Array.isArray(maybeDate)) {
+      return maybeDate[0]
+    }
+    return undefined
   }
 
   return (
@@ -103,6 +132,40 @@ const EditTaskModal: FunctionComponent = () => {
                     value={tags.join(' ')}
                     placeholder="tag1 tag2 tag3"
                     onChange={(e) => setTags(e.target.value.split(' '))}
+                  />
+                </label>
+              </Cell>
+            </Grid>
+            <Grid>
+              <Cell medium={4}>
+                <label>
+                  Due
+                  <DatePicker
+                    selected={due}
+                    showTimeSelect={true}
+                    onChange={(date) => setDue(getSingleDateOrUndefined(date))}
+                  />
+                </label>
+              </Cell>
+              <Cell medium={4}>
+                <label>
+                  Wait
+                  <DatePicker
+                    selected={wait}
+                    showTimeSelect={true}
+                    onChange={(date) => setWait(getSingleDateOrUndefined(date))}
+                  />
+                </label>
+              </Cell>
+              <Cell medium={4}>
+                <label>
+                  Scheduled
+                  <DatePicker
+                    selected={scheduled}
+                    showTimeSelect={true}
+                    onChange={(date) =>
+                      setScheduled(getSingleDateOrUndefined(date))
+                    }
                   />
                 </label>
               </Cell>
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
