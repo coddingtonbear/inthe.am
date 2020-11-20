4f548620413ca3e0e214c5579af5b38143dc684f
Adam Coddington <me@adamcoddington.net>
2020-11-19 17:55:02 -0800

    Adding synchronization section for control panel.

--- ui/src/components/configure_pages/Synchronization.tsx
+++ ui/src/components/configure_pages/Synchronization.tsx
@@ -1,7 +1,26 @@
 import React, {FunctionComponent} from 'react'
 
+import SyncInstructions from '../SyncInstructions'
+
 const Synchronization: FunctionComponent = () => {
-  return <div>Synchronization Panel</div>
+  return (
+    <div id="custom_taskd" className="content">
+      <div className="row">
+        <div className="large-12 columns">
+          <p>
+            Inthe.AM provides a built-in taskserver for you to synchronize your
+            tasks with; follow the instructions below for setting up your local
+            taskwarrior client to synchronize with Inthe.AM.
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="large-12 columns">
+          <SyncInstructions />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Synchronization
