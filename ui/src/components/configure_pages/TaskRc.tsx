bd6ca0094efec2df18a08d98c73ba8aeb0fe4076
Adam Coddington <me@adamcoddington.net>
2020-11-20 18:22:10 -0800

    Adding API Keys & Taskrc.

---
+++ ui/src/components/configure_pages/TaskRc.tsx
@@ -0,0 +1,56 @@
+import React, {ChangeEvent, FunctionComponent} from 'react'
+import {Button} from 'react-foundation'
+import {useSelector} from 'react-redux'
+import {useToasts} from 'react-toast-notifications'
+
+import request from '../../clients/request'
+import {RootState, useAppDispatch} from '../../store'
+import {refreshStatus} from '../../thunks/status'
+
+const TaskRc: FunctionComponent = () => {
+  const taskRcData = useSelector((state: RootState) =>
+    state.status.logged_in ? state.status.taskrc_extras : ''
+  )
+  const taskrcExtrasUrl = useSelector((state: RootState) =>
+    state.status.logged_in ? state.status.urls?.taskrc_extras : null
+  )
+  const [taskRc, setTaskRc] = React.useState<string>(taskRcData)
+  const {addToast} = useToasts()
+  const dispatch = useAppDispatch()
+
+  function onChangeTaskrc(event: ChangeEvent<HTMLTextAreaElement>) {
+    setTaskRc(event.target.value)
+  }
+
+  function onSaveTaskrc() {
+    if (!taskrcExtrasUrl) {
+      throw new Error('No taskrc extras URL found')
+    }
+
+    request('PUT', taskrcExtrasUrl, {
+      data: taskRc,
+      lookupApiUrl: false,
+    }).then(() => {
+      dispatch(refreshStatus())
+      addToast('TaskRc Extras updated.', {appearance: 'success'})
+    })
+  }
+
+  return (
+    <div className="row">
+      <div className="large-12 columns">
+        <h3>TaskRc Extras</h3>
+        <textarea onChange={onChangeTaskrc} value={taskRc} />
+        <p className="input-note">
+          Only configuration values relating to urgency or UDA definitions will
+          have an effect, but entering your entire local{' '}
+          <span className="code">.taskrc</span>
+          is both safe and encouraged.
+        </p>
+        <Button onClick={onSaveTaskrc}>Save Taskrc</Button>
+      </div>
+    </div>
+  )
+}
+
+export default TaskRc
