b9252d4c15e72a4c7d14f7d7ecae3089c8b9604a
Adam Coddington <me@adamcoddington.net>
2021-07-31 16:42:31 -0700

    [#358] Rebuilding migrations; adding management command for correcting missing taskstore accounts.

--- inthe_am/taskmanager/management/commands/taskstore.py
+++ inthe_am/taskmanager/management/commands/taskstore.py
@@ -1,8 +1,10 @@
 from contextlib import contextmanager
 import datetime
+import json
 import os
 import traceback
 import uuid
+from typing import cast
 
 import progressbar
 
@@ -66,7 +68,7 @@ class Command(BaseCommand):
                 "squash",
                 "delete_old_accounts",
                 "list_old_accounts",
-                "migrate_all",
+                "create_taskstore_accounts",
             ],
             type=str,
         )
@@ -137,6 +139,42 @@ class Command(BaseCommand):
                         run_id=run_id,
                     )
                     bar.update(idx)
+        elif subcommand == "create_taskstore_accounts":
+            for store in TaskStore.objects.order_by("-last_synced"):
+                store = cast(TaskStore, store)
+                print(store)
+
+                if not store.taskd_account.exists():
+                    try:
+                        user_key = store.taskrc["taskd.credentials"].split("/")[2]
+
+                        store.taskd_account.make_user_request(
+                            "PUT", data=json.dumps({"user_key": user_key,})
+                        )
+                        print("> ACCOUNT CREATION OK")
+                    except Exception:
+                        print("> ACCOUNT CREATION FAILED")
+                        continue
+                else:
+                    print("> ACCOUNT EXISTS")
+
+                certs = store.taskd_account.make_user_request(
+                    "GET", "certificates"
+                ).json()
+                if not certs:
+                    try:
+                        cert_fingerprint = store.taskrc.get_certificate_fingerprint()
+                        store.taskd_account.make_user_request(
+                            "PUT",
+                            data=json.dumps({}),
+                            path=f"certificates/{cert_fingerprint}",
+                        )
+                        print("> CERT SET OK")
+                    except Exception:
+                        print("> CERT SET FAILED")
+                        continue
+                else:
+                    print("> CERT ALREADY SET")
         elif subcommand == "gc_large_repos":
             for store in TaskStore.objects.order_by("-last_synced"):
                 try:
