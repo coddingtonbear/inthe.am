33581a344c6401dab8b57277882cea73d3c652b2
Adam Coddington <me@adamcoddington.net>
2022-02-05 17:04:50 -0800

    Do not attempt to backfill task history for accounts that have no account yet.

--- inthe_am/taskmanager/models/task.py
+++ inthe_am/taskmanager/models/task.py
@@ -7,6 +7,7 @@ from django.db import models, transaction
 from .changesource import ChangeSource
 from .taskstore import TaskStore
 from .taskdata import TaskData
+from ..exceptions import InvalidTaskwarriorConfiguration
 
 
 class Task(models.Model):
@@ -51,19 +52,23 @@ class Task(models.Model):
 
     @classmethod
     def backfill_task_records(cls, store: TaskStore):
-        with transaction.atomic():
-            source = ChangeSource.objects.create(
-                store=store,
-                sourcetype=ChangeSource.SOURCETYPE_BACKFILL,
-                commit_hash=store.repository.head().decode("utf-8"),
-            )
-
-            for task in store.client.filter_tasks({}):
-                cls.record_change(
-                    store,
-                    source,
-                    data=task.serialized(),
+        try:
+            with transaction.atomic():
+                source = ChangeSource.objects.create(
+                    store=store,
+                    sourcetype=ChangeSource.SOURCETYPE_BACKFILL,
+                    commit_hash=store.repository.head().decode("utf-8"),
                 )
+
+                for task in store.client.filter_tasks({}):
+                    cls.record_change(
+                        store,
+                        source,
+                        data=task.serialized(),
+                    )
+        except InvalidTaskwarriorConfiguration:
+            # This happens if the user doesn't have an account yet!
+            pass
 
     def __str__(self):
         return f"Task {self.task_id} for {self.store}"
